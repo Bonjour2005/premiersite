35593daf4317976cfbc21da98f875bbb88baf140
Bonjour2005 <enzo.carle92@gmail.com>
2022-01-21 16:10:53 +0100

    ajout de slider et page de connexion

---
+++ slider.js
@@ -0,0 +1,72 @@
+const items = document.querySelectorAll('img');
+const nbSlide = items.length;
+const suivant = document.querySelector('.right');
+const precedent = document.querySelector('.left');
+let count = 0;
+
+function slideSuivante(){
+    items[count].classList.remove('active');
+
+    if(count < nbSlide - 1){
+        count++;
+    } else {
+        count = 0;
+    }
+
+    items[count].classList.add('active')
+    console.log(count);
+    
+}
+suivant.addEventListener('click', slideSuivante)
+
+
+function slidePrecedente(){
+    items[count].classList.remove('active');
+
+    if(count > 0){
+        count--;
+    } else {
+        count = nbSlide - 1;
+    }
+
+    items[count].classList.add('active')
+    // console.log(count);
+    
+}
+precedent.addEventListener('click', slidePrecedente)
+
+function keyPress(e){
+    console.log(e);
+    
+    if(e.keyCode === 37){
+        slidePrecedente();
+    } else if(e.keyCode === 39){
+        slideSuivante();
+    }
+}
+document.addEventListener('keydown', keyPress)
+
+let tabs = document.querySelectorAll(".tab-link:not(.desactive)");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    unSelectAll();
+    tab.classList.add("active");
+    let ref = tab.getAttribute("data-ref");
+    document
+      .querySelector(`.tab-body[data-id="${ref}"]`)
+      .classList.add("active");
+  });
+});
+
+function unSelectAll() {
+  tabs.forEach((tab) => {
+    tab.classList.remove("active");
+  });
+  let tabbodies = document.querySelectorAll(".tab-body");
+  tabbodies.forEach((tab) => {
+    tab.classList.remove("active");
+  });
+}
+
+document.querySelector(".tab-link.active").click();
